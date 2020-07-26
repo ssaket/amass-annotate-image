737b6e361e98799ea80068f4dceeccec44881e4c
Shubham Goyal <shubh1993goyal@gmail.com>
2020-07-26 13:25:47 -0700

    Added bootstrap and Font awesome

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SearchBar from "./components/SearchBar";
+function App() {
+  return (
+    <div className="image-amass-app container">
+      <SearchBar></SearchBar>
+    </div>
+  );
+}
+
+export default App;
