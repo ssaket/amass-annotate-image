e48bbeb7dfab961594b5f5c9b9aa9351c3a430e5
Shubham Goyal <shubh1993goyal@gmail.com>
2020-07-26 13:50:08 -0700

    Axios installation

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function SearchBar() {
   return (
-    <div>
-      <input type="text" className="" />
+    <div className="search">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Enter an image to be searched...   "
+      />
     </div>
   );
 }
