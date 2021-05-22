3f442fde437f52badd0bb2f496d4995d397711ea
ssaket <skt.saurabh@gmail.com>
2021-05-22 15:19:51 +0200

    new filter component

---
+++ src/components/app-image/ImageFilter.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+
+const ImageFilter = ({ images, count, setCount }) => {
+
+    const [isChecked, setCheckbox] = useState(false);
+
+    const onChange = e => {
+        setCheckbox(e.target.checked);
+        let curr_cnt = 0;
+        for (const image of images) {
+            curr_cnt++;
+            image.checked = e.target.checked;
+            console.log(image, image.checked);
+        }
+
+        e.target.checked ? setCount(curr_cnt) : setCount(0);
+    }
+
+    return (
+        <React.Fragment>
+            <form className="my-4">
+                <fieldset>
+                    <legend>filters</legend>
+                    <div className="mb-3">
+                        <label htmlFor="description" className="form-label">Search for Description</label>
+                        <input type="text" id="description" className="form-control form-control-sm" placeholder="Search" />
+                    </div>
+                    <div className="mb-3">
+                        <div class="row">
+                            <div className="col">
+                                <label htmlFor="imageperpages" className="form-label">Items per page </label>
+                            </div>
+                            <div className="col">
+                                <select id="imageperpages" className="form-select form-select-sm">
+                                    <option>10</option>
+                                    <option>20</option>
+                                    <option>30</option>
+                                    <option>50</option>
+                                    <option>100</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="form-check">
+                            <input onChange={onChange} checked={isChecked} className="form-check-input" type="checkbox" id="selectAll" />
+                            <label className="form-check-label" htmlFor="selectAll">
+                                Select All
+                            </label>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="form-text">
+                            <p>Total images: {images.length}</p>
+                            <p>Selected: {count}</p>
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Annotate</button>
+                </fieldset>
+            </form>
+        </React.Fragment>
+    )
+}
+
+export default ImageFilter
