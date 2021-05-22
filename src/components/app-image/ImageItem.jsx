42040765cc5be8c364f3524e1f48665966c60fac
ssaket <skt.saurabh@gmail.com>
2021-05-22 15:20:41 +0200

    add count, total count

--- src/components/app-image/ImageItem.jsx
+++ src/components/app-image/ImageItem.jsx
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const ImageItem = ({ image }) => {
+const ImageItem = ({ image, count }) => {
 
     const { id, src, checked } = image;
     const [isChecked, setChecked] = useState(false);
 
     const onChange = e => {
+        if(e.target.checked) 
+            count++;
+        else
+            count--;
         setChecked(e.target.checked);
     }
 
     return (
         <React.Fragment>
-            <div id={id} className="card m-3 px-3" style={{ maxWidth: '18rem' }}>
-                <img src={src} loading="lazy" className="card-img-top" alt={id} />
+            {/* <div id={id} className="card m-3 px-3" style={{ maxWidth: '18rem' }}>
+                <img src={src} loading="lazy" className="rounded card-img-top" alt={id} />
                 <div className="card-body">
                     <div className="card-text">
                         <div className="row">
@@ -31,6 +35,19 @@ const ImageItem = ({ image }) => {
                         </div>
                     </div>
                 </div>
+            </div> */}
+            <div class="m-2 px-3">
+                <figure className="figure">
+                    <img src={src} loading="lazy"  style={{ maxWidth: '15rem' }} className="figure-img img-fluid rounded" alt="..." />
+                    <figcaption className="figure-caption text-end">
+                        <div className="form-check">
+                            <input onChange={onChange} className="form-check-input" type="checkbox" value="" id={'src_' + id} checked={checked ? checked : isChecked} />
+                            <label className="form-check-label" htmlFor={id}>
+                                Annotate
+                            </label>
+                        </div>
+                    </figcaption>
+                </figure>
             </div>
         </React.Fragment>
     );
